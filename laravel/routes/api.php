e9695b9e15e49146e6aa91c45b57e812c351aa51
thy-dionmar <thy.dionmar@gmail.com>
2023-06-15 16:57:37 +0800

    set todos api routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\CompleteTodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::apiResource('todos', TodoController::class);
+Route::patch('todos/{todo}/complete', CompleteTodoController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
